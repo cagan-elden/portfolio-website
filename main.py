51dc77e2525b8270acfa0504b9596d3f1ee21fab
Cagan Eldenk <caganeldenk@gmail.com>
2024-12-16 22:29:13 +0300

    Created the layout of the portfolio in flask

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
