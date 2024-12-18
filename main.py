3b34549afa11e985a9ce20fa124cc3d0851f5ca6
Cagan Eldenk <caganeldenk@gmail.com>
2024-12-18 21:36:37 +0300

    base.html created home.html got seperated for better workflow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
